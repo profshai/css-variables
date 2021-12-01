bb74048ab0dc88953748a6778bf3697241903f86
Shaibu Yahaya <62988498+profshai@users.noreply.github.com>
2021-12-01 10:58:58 -0600

    css-variables

---
+++ variables.js
@@ -0,0 +1,8 @@
+// Using javascript to change colors
+// changing red to blue
+
+// let root = document.querySelector(':root');
+// let rootStyles = getComputedStyle(root);
+// let red = rootStyles.getPropertyValue('--red')
+//
+// root.style.setProperty('--red', 'blue')
